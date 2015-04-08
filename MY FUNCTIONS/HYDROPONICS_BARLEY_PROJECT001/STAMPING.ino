6b136cb450c1bd5be07fe6c940d4103a7339ebbb
abumarcusvsadnik <abumarcusvsadnik@gmail.com>
2015-04-08 21:18:32 +0300

    OK

--- MY FUNCTIONS/HYDROPONICS_BARLEY_PROJECT001/STAMPING.ino
+++ MY FUNCTIONS/HYDROPONICS_BARLEY_PROJECT001/STAMPING.ino
@@ -176,3 +176,114 @@ int createStampFile(int tmHour, int tmMinute, int tmSecond, int tmDay, int tmMon
 
   }
   
+  
+  
+//!--------------------------------------------------------------------------------------------!//  
+  int readStampReturntmHour(char* filename) {
+  SD.begin(chipSelect);
+  int i,h, h0, h1, h2;
+
+  // re-open the file for reading:
+  File myFile = SD.open(filename);
+  int stamp[13];
+  if (myFile) {
+    
+    Serial.println("<!--------------------------------------------------------!>");
+    Serial.println("(*)readStampReturntmHour()");
+
+    Serial.print("\tFilename\t: ");
+    Serial.println(filename);
+    
+    // read from the file until there's nothing else in it:
+    
+
+   i = 0;
+   while(myFile.available()){
+   
+    stamp[i] = myFile.read();   
+    i++;
+   }
+    
+
+    // close the file:
+    myFile.close();
+
+  } else {
+    // if the file didn't open, print an error:
+    Serial.print("\nerror opening\t");
+    Serial.println(filename);
+    Serial.println();
+  }
+
+Serial.println("\n\nHour Value is: ");
+for(i=0;i  <= 13 ;i++){
+
+Serial.print("Stamp[");
+Serial.print(i);
+Serial.print("] = [");
+Serial.print(stamp[i]);
+Serial.print("]");
+Serial.println("");
+}
+
+Serial.println();
+Serial.println("<!--------------------------------------------------------!>");
+
+
+
+h0 = asciitodec(stamp[0]);         
+        
+h1 = asciitodec(stamp[1]);  
+        
+        h2 = h0*10 + h1*1;
+        
+        Serial.print("h0 ");
+        Serial.println(h0);        
+        Serial.print("h1 ");
+        Serial.println(h1);        
+        Serial.print("h2 ");
+        Serial.println(h2);        
+        
+        
+        
+        return h2;
+
+}//end function
+//!--------------------------------------------------------------------------------------------!//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+int asciitodec(int ascii){ 
+
+int h0;
+ 
+switch(ascii){
+   case 48 : h0 = 0; break;
+   case 49 : h0 = 1; break;
+   case 50 : h0 = 2;  break;
+   case 51 : h0 = 3;  break;
+    case 52 : h0 = 4;  break;
+     case 53 : h0 = 5;  break;
+      case 54 : h0 = 6;  break;
+       case 55 : h0 = 7;  break;
+        case 56 : h0 = 8;  break;
+         case 57 : h0 = 9;  break;           
+}
+
+return h0;
+
+}//end function
